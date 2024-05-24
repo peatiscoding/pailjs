9eeda547bc5b76d2d6466cbe75d63d99ed5b1fc2
Kittiphat S <freeuxer@gmail.com>
2024-05-24 09:41:40 +0700

    feat: enhance zodSchema to aware of the incoming type

--- library/pailjs/src/marshal/zod.ts
+++ library/pailjs/src/marshal/zod.ts
@@ -4,5 +4,8 @@ import type { z } from 'zod'
 export const zodSchema =
   <I, T>(zodSchema: z.ZodType<T>): MarshalTypeMorpher<I, z.infer<typeof zodSchema>> =>
   async (result, _resopnse, _op): Promise<z.infer<typeof zodSchema>> => {
+    if (result instanceof Response) {
+      return zodSchema.parse(await result.json())
+    }
     return zodSchema.parse(result)
   }
