4a214ed43eb17ddd82fb2d183a6d80d327b52f42
Kittiphat S <freeuxer@gmail.com>
2024-05-26 22:28:55 +0700

    chore: update zod type reference

--- library/pailjs/src/marshal/zod.ts
+++ library/pailjs/src/marshal/zod.ts
@@ -2,7 +2,7 @@ import type { MarshalTypeMorpher } from '../interface'
 import type { z } from 'zod'
 
 export const zodSchema =
-  <I, T>(zodSchema: z.ZodType<T>): MarshalTypeMorpher<I, z.infer<typeof zodSchema>> =>
+  <I, T>(zodSchema: z.ZodType<T, z.ZodTypeDef, any>): MarshalTypeMorpher<I, z.infer<typeof zodSchema>> =>
   async (result, _resopnse, _op): Promise<z.infer<typeof zodSchema>> => {
     if (result instanceof Response) {
       return zodSchema.parse(await result.json())
